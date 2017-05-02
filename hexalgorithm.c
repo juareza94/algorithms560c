3be8e18a9e8c6740d73bc57807789c3f2c759e9d
juareza94 <thejuareza@icloud.com>
2017-05-02 13:06:38 -0700

    Update hexalgorithm.c
    Table is made and runs. Use ./a.out INT to test hexagons using the graph.

--- hexalgorithm.c
+++ hexalgorithm.c
@@ -9,16 +9,12 @@
  * Compile using: gcc hexalgorithm.c
  *
  * Array of neighbors is defined as such
- * [upleft,up,upright,downleft,down,downright]
+ * [0-upleft, 1-up, 2-upright, 3-downleft, 5-down, 5-downright]
 \******************************************************************/
 
 /******************************************************************\
  * Current Tasks:
- * 
- * Line 111,116,123: I am trying to dynamically create a new hexagon
- * at each run of the for loop but currently, syntax is producing
- * error: request for member `y/weight' in something not a structure or union
- *
+ * dijkstra
 \*******************************************************************/
 #include <stdio.h>
 #include <sys/types.h>
@@ -33,16 +29,93 @@
 #define FALSE 0
 #define MAXINT 9999
 
+//Define Hexagon Cell
 struct hexdef{
   int y;                          /* hexagon number */
   int weight;                     /* hexagon weight */
   struct hexdef* next[6];         /* array of neighbors */
 };
+//Define Graph of Hexagon Cells
 struct hexgraph{
   struct hexdef* vertices[MAXV];           /* adj list for each vertex */
   int nvertices;                        /* number of vertices in graph */
   int nedges;                           /* number of edges in graph*/
 };
+
+void dijkstra(struct hexgraph* g, int start);
+void testPrinter(int i);
+struct hexdef hexagon[MAXV];
+struct hexgraph graph;
+
+int main(int argc, char* argv[]) {
+  //Open and direct the file to STDIN
+  char* input = "INPUT.TXT";
+  int input_fd = open(input, O_RDONLY);
+  dup2(input_fd,STDIN);
+
+  //Read the file and create nodes
+  int i,x;
+  char c;
+  char number[3];
+
+  //Set properties of the graph
+  graph.nvertices = MAXV;
+  graph.nedges = MAXV;
+
+  for (i=1;i<=233;i++) {
+    //Create the new hexagon and store to graph list of vertices
+    graph.vertices[i] = &hexagon[i];
+    //printf("%p\n", graph.vertices[i]);
+
+    x = 0;
+    //Grab and set node number
+    while(isdigit(c = getchar())) {
+      x = (x * 10) + (c - '0');
+    } hexagon[i].y = x;
+    //printf("Node is: %d     ", hexagon[i].y);
+
+    //Grab and set weight number
+    x = 0;
+    while(isdigit(c = getchar())) {
+      x = (x*10) + (c - '0');
+    } hexagon[i].weight = x;
+    //printf("Cost is: %d \n", hexagon[i].weight);
+  }
+  close(input_fd);
+
+  //Set children for Each Hexagon
+  for (i=1; i<=MAXV; i++){
+    //top left
+    if (i < 9 || (i-1)%15==0) hexagon[i].next[0] = NULL;
+    else hexagon[i].next[0] = &hexagon[i-8];
+    //top
+    if (i<16) hexagon[i].next[1] = NULL;
+    else hexagon[i].next[1] = &hexagon[i-15];
+    //top right
+    if (i < 9 || (i-8)%15==0) hexagon[i].next[2] = NULL;
+    else hexagon[i].next[2] = &hexagon[i-7];
+    //bottom left
+    if (i > 227 || (i-1)%15==0) hexagon[i].next[3] = NULL;
+    else hexagon[i].next[3] = &hexagon[i+7];
+    //bottom
+    if (i>218) hexagon[i].next[4] = NULL;
+    else hexagon[i].next[4] = &hexagon[i+15];
+    //bottom right
+    if (i>225 || (i-8)%15==0) hexagon[i].next[5] = NULL;
+    else hexagon[i].next[5] = &hexagon[i+8];
+  }
+
+  if (argv[1]!=NULL) {
+    int test = 0,i;
+    for (i = 0;argv[1][i]!=NULL;i++){
+      char c = argv[1][i];
+      test = test*10 + c-'0';
+    }
+    testPrinter(test);
+  }
+  //Find the Shortest Path
+  //dijkstra()
+}
 
 void dijkstra(struct hexgraph* g, int start) {
   int i;
@@ -86,48 +159,26 @@ void dijkstra(struct hexgraph* g, int start) {
   }
 }
 
-int main() {
-  //Open and direct the file to STDIN
-  char* input = "INPUT.TXT";
-  char* output = "OUTPUT.TXT";
-  int input_fd = open(input, O_RDONLY);
-  dup2(input_fd,STDIN);
-
-  //Read the file and create nodes
-  int i,x;
-  char c;
-  char number[3];
-
-  //Define the graph
-  struct hexgraph graph;
-  graph.nvertices = 233;
-  graph.nedges = 233;
-
-  //Create Each Node
-  for (i=1;i<=233;i++) {    
-    //Create the new hexagon and store to graph list of vertices
-    //struct hexdef hexagon;                              //Original definition of hexagon here
-    x = 0;
-    graph.vertices[i] = malloc(sizeof(struct hexdef));    //Attempting new hexagon* and set to graph.vertices[i]
-
-    //Grab and set node number
-    while(isdigit(c = getchar())) {
-      x = (x*10) + (c - '0');
-    } graph.vertices[i].y = x;
-    //printf("Node is: %d     ", *graph.vertices[i].y);
-
-    //Grab and set weight number
-    x = 0;
-    while(isdigit(c = getchar())) {
-      x = (x*10) + (c - '0');
-    } graph.vertices[i].weight = x;
-    //printf("Cost is: %d \n", graph.vertices[i].weight);
-  }
-  close(input_fd);
-
-  //Assign Each Node Its Children
-  //IN PROGRESS
-
-  //Find the Shortest Path
-  //dijkstra()
+void testPrinter(int i){
+  printf("Hexagon %d in grid looks like:\n\n", i);
+  //TOP
+  if (hexagon[i].next[1]==NULL)  printf("     NULL\n");
+  else printf("       %d\n", graph.vertices[i]->next[1]->y);
+  if (hexagon[i].next[1]==NULL) printf("      __     \n");
+  else printf("        __     \n");
+  //TOP LEFT
+  if(hexagon[i].next[0] == NULL) printf("NULL/    ");
+  else printf("   %d/    ", graph.vertices[i]->next[0]->y);
+  //TOP RIGHT
+  if (hexagon[i].next[2] == NULL) printf("\\NULL\n");
+  else printf("\\%d \n", graph.vertices[i]->next[2]->y);
+  //BOTTOM LEFT
+  if (hexagon[i].next[3] == NULL) printf("NULL\\ __ ");
+  else printf("   %d\\ __ ",graph.vertices[i]->next[3]->y);
+  //BOTTOM RIGHT
+  if (hexagon[i].next[5] == NULL) printf("/NULL \n");
+  else printf("/%d \n", graph.vertices[i]->next[5]->y);
+  //BOTTOM
+  if(hexagon[i].next[4] == NULL) printf("    NULL \n");
+  else printf("       %d \n", graph.vertices[i]->next[4]->y);
 }
